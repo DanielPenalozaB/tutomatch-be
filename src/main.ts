e4213ee458adc5c65b158cb4f8b69cda522ed8df
Daniel Peñaloza <juandanielpenalozabrito@gmail.com>
2025-05-01 07:54:43 -0500

    Integrate the environment variable APP_PORT

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('APP_PORT', 4000);
 
   // Swagger configuration
   const config = new DocumentBuilder()
@@ -17,6 +20,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(4000);
+  await app.listen(port);
 }
 bootstrap().catch(console.error);
