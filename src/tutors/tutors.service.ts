aaee6e8cd7d172b52b50cd9cd2a63a3051ea6483
danieltoo <danielduranmadrid71@gmail.com>
2025-05-17 23:39:41 -0500

    añadir remove

--- src/tutors/tutors.service.ts
+++ src/tutors/tutors.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTutorDto } from './dto/create-tutor.dto';
 import { UpdateTutorDto } from './dto/update-tutor.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Availability } from 'src/availabilities/entities/availability.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class TutorsService {
+  constructor(
+    @InjectRepository(Availability)
+    private availabilityRepository: Repository<Availability>
+  ) { }
+  
   create(createTutorDto: CreateTutorDto) {
     return 'This action adds a new tutor';
   }
@@ -20,7 +28,11 @@ export class TutorsService {
     return `This action updates a #${id} tutor`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} tutor`;
+  async remove(id: number): Promise<void> {
+  const result = await this.availabilityRepository.delete(id);
+
+  if (result.affected === 0) {
+    throw new NotFoundException(`No availability found with ID ${id}`);
   }
+}
 }
