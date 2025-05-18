d0849c684e6aeaa95e7897bff94d716cbbcbaa74
danieltoo <danielduranmadrid71@gmail.com>
2025-05-18 02:34:40 -0500

    add metodos para el calendario

--- src/tutors/tutors.service.ts
+++ src/tutors/tutors.service.ts
@@ -1,17 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CreateTutorDto } from './dto/create-tutor.dto';
 import { UpdateTutorDto } from './dto/update-tutor.dto';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Availability } from 'src/availabilities/entities/availability.entity';
-import { Repository } from 'typeorm';
 
 @Injectable()
 export class TutorsService {
-  constructor(
-    @InjectRepository(Availability)
-    private availabilityRepository: Repository<Availability>
-  ) { }
-  
   create(createTutorDto: CreateTutorDto) {
     return 'This action adds a new tutor';
   }
@@ -28,11 +20,7 @@ export class TutorsService {
     return `This action updates a #${id} tutor`;
   }
 
-  async remove(id: number): Promise<void> {
-  const result = await this.availabilityRepository.delete(id);
-
-  if (result.affected === 0) {
-    throw new NotFoundException(`No availability found with ID ${id}`);
+  remove(id: number) {
+    return `This action removes a #${id} tutor`;
   }
-}
 }
